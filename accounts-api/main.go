ee08ac871c28b228c909e1ad0571d688d2ac4f49
florianwoelki <florianwoelki@gmx.de>
2020-11-21 16:48:04 +0100

    initial commit

---
+++ accounts-api/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
+
+var address = ":9090"
+
+func main() {
+	logger := log.New(os.Stdout, "accounts-api ", log.LstdFlags)
+
+	// create gorilla mux router
+	router := mux.NewRouter()
+
+	// create a new server
+	server := http.Server{
+		Addr:         address,
+		Handler:      router,
+		ErrorLog:     logger,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+
+	// start the server
+	go func() {
+		logger.Println("Starting server on port", address)
+
+		err := server.ListenAndServe()
+		if err != nil {
+			logger.Printf("Error starting server: %s\n", err)
+			os.Exit(1)
+		}
+	}()
+
+	// gracefully shutdown the server and catch interrupt or kill
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, os.Kill)
+
+	// block until signal is received
+	sig := <-c
+	log.Println("Got signal:", sig)
+
+	// gracefully shutdown the server, waiting max 30 seconds for current operations to complete
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	server.Shutdown(ctx)
+}
