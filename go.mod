dafb5e4c43fb06e150c74c91c329bc630877cf9e
florianwoelki <florianwoelki@gmx.de>
2020-11-23 08:43:29 +0100

    feat(db): add validation to User struct

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/florianwoelki/insta-clone
 go 1.15
 
 require (
+	github.com/go-playground/validator/v10 v10.4.1
 	github.com/gorilla/mux v1.8.0
 	gorm.io/driver/postgres v1.0.5
 	gorm.io/gorm v1.20.7
--- internal/types.go
+++ internal/types.go
@@ -3,17 +3,25 @@ package internal
 import (
 	"database/sql"
 	"time"
+
+	"github.com/go-playground/validator/v10"
 )
 
 // User is the database struct
 type User struct {
 	ID          uint         `json:"id"`
-	Name        string       `json:"name"`
-	Email       *string      `json:"email"`
-	Username    string       `json:"username"`
-	Age         uint8        `json:"age"`
-	Birthday    *time.Time   `json:"birthday"`
+	Name        string       `json:"name" validate:"required"`
+	Email       *string      `json:"email" validate:"required"`
+	Username    string       `json:"username" validate:"required"`
+	Age         uint8        `json:"age" validate:"required"`
+	Birthday    *time.Time   `json:"birthday" validate:"required"`
 	ActivatedAt sql.NullTime `json:"-"`
 	CreatedAt   time.Time    `json:"-"`
 	UpdatedAt   time.Time    `json:"-"`
+}
+
+// Validate checks the validation of the user struct
+func (u *User) Validate() error {
+	validate := validator.New()
+	return validate.Struct(u)
 }
