df1002c303c7aea3e3bb9ffa14978f5dd38826b6
florianwoelki <florianwoelki@gmx.de>
2020-11-26 14:01:31 +0100

    feat(account-api): add middleware for cookie token validation

--- account-api/handlers/middleware.go
+++ account-api/handlers/middleware.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"net/http"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/florianwoelki/insta-clone/internal"
 )
 
@@ -24,6 +25,53 @@ func (a *Accounts) MiddlewareValidateAccount(next http.Handler) http.Handler {
 
 		ctx := context.WithValue(r.Context(), &KeyAccount{}, account)
 		r = r.WithContext(ctx)
+
+		next.ServeHTTP(rw, r)
+	})
+}
+
+// MiddlewareValidateToken validates if the jwt in the cookie is valid
+func (a *Accounts) MiddlewareValidateToken(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		rw.Header().Add("Content-Type", "application/json")
+
+		c, err := r.Cookie("token")
+		if err != nil {
+			if err == http.ErrNoCookie {
+				a.logger.Printf("[ERROR] Cookie is not set, error %v", err)
+				http.Error(rw, "Not authorized", http.StatusUnauthorized)
+				return
+			}
+			a.logger.Printf("[ERROR] Something went wrong while fetching the token cookie, error %v", err)
+			http.Error(rw, "Something internally went wrong", http.StatusBadRequest)
+			return
+		}
+
+		tokenStr := c.Value
+
+		claims := &internal.Claims{}
+
+		token, err := jwt.ParseWithClaims(tokenStr, claims, func(token *jwt.Token) (interface{}, error) {
+			return internal.JWTKey, nil
+		})
+		if err != nil {
+			if err == jwt.ErrSignatureInvalid {
+				a.logger.Printf("[ERROR] Invalid token, error: %v", err)
+				http.Error(rw, "Invalid token", http.StatusUnauthorized)
+				return
+			}
+
+			a.logger.Printf("[ERROR] Something went wrong while validating the token cookie, error: %v", err)
+			http.Error(rw, "Something went wrong internally", http.StatusBadRequest)
+			return
+		}
+
+		// check if token is invalid
+		if !token.Valid {
+			a.logger.Println("[ERROR] Invalid token")
+			http.Error(rw, "Invalid token", http.StatusUnauthorized)
+			return
+		}
 
 		next.ServeHTTP(rw, r)
 	})
