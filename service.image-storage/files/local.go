6a57ee2cfa4964f69f297c0d6f7f0bc06e232e71
florianwoelki <florianwoelki@gmx.de>
2020-12-01 08:32:51 +0100

    docs(service): commented files for image service

--- service.image-storage/files/local.go
+++ service.image-storage/files/local.go
@@ -8,11 +8,14 @@ import (
 	"golang.org/x/xerrors"
 )
 
+// Local struct represents a local storage
 type Local struct {
 	maxFileSize int
 	basePath    string
 }
 
+// NewLocal creates a new local struct with given basePath and maxSize of
+// the files
 func NewLocal(basePath string, maxSize int) (*Local, error) {
 	path, err := filepath.Abs(basePath)
 	if err != nil {
@@ -22,17 +25,22 @@ func NewLocal(basePath string, maxSize int) (*Local, error) {
 	return &Local{basePath: path}, nil
 }
 
+// Save is implemented from the Storage interface and it will save contents to
+// a specific file.
 func (l *Local) Save(path string, contents io.Reader) error {
 	localFilePath := l.fullPath(path)
 
+	// check dir of localFilePath and create it
 	dir := filepath.Dir(localFilePath)
 	err := os.MkdirAll(dir, os.ModeDir)
 	if err != nil {
 		return xerrors.Errorf("Unable to create directory: %w", err)
 	}
 
+	// get the file info of the localFilePath
 	_, err = os.Stat(localFilePath)
 	if err == nil {
+		// try to remove the file for saving later
 		err = os.Remove(localFilePath)
 		if err != nil {
 			return xerrors.Errorf("Unable to delete file: %w", err)
@@ -41,12 +49,14 @@ func (l *Local) Save(path string, contents io.Reader) error {
 		return xerrors.Errorf("Unable to get file info: %w", err)
 	}
 
+	// create new file with localFilePath
 	file, err := os.Create(localFilePath)
 	if err != nil {
 		return xerrors.Errorf("Unable to create file: %w", err)
 	}
 	defer file.Close()
 
+	// copy the given contents into the created file
 	_, err = io.Copy(file, contents)
 	if err != nil {
 		return xerrors.Errorf("Unable to write to file: %w", err)
@@ -55,9 +65,11 @@ func (l *Local) Save(path string, contents io.Reader) error {
 	return nil
 }
 
+// Get returns contents of a specific path or file
 func (l *Local) Get(path string) (*os.File, error) {
 	filepath := l.fullPath(path)
 
+	// open file and catch error
 	file, err := os.Open(filepath)
 	if err != nil {
 		return nil, xerrors.Errorf("Unable to open file: %w", err)
--- service.image-storage/files/storage.go
+++ service.image-storage/files/storage.go
@@ -2,6 +2,7 @@ package files
 
 import "io"
 
+// Storage interface will represent a storage for this application
 type Storage interface {
 	Save(path string, file io.Reader) error
 }
