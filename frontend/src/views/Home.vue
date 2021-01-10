95227339e6b06b15498fd729f275c3fa202f416a
florianwoelki <florianwoelki@gmx.de>
2021-01-10 11:12:36 +0100

    chore(ui): notifications and messages are links

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -90,14 +90,14 @@
 
         <!-- RIGHT SECTION -->
         <div class="flex items-center space-x-6">
-          <div class="relative text-gray-600">
+          <a href="#" class="relative text-gray-600">
             <Icon name="bell" class="w-5 h-5" />
             <div class="absolute top-0 right-0 w-1 h-1 -mt-1 -mr-1 bg-red-600 rounded-full" />
-          </div>
-          <div class="relative text-gray-600">
+          </a>
+          <a href="#" class="relative text-gray-600">
             <Icon name="mail" class="w-5 h-5" />
             <div class="absolute top-0 right-0 w-1 h-1 -mt-1 -mr-1 bg-red-600 rounded-full" />
-          </div>
+          </a>
           <Button>
             <Icon name="plus-circle" class="w-6 h-6" />
             <span>Add photo</span>
