6683da2d1ca7a05923edec27455e43255f220fd4
florianwoelki <florianwoelki@gmx.de>
2021-01-20 19:04:27 +0100

    feat(notifications): add basic websocket connection

--- go.mod
+++ go.mod
@@ -11,10 +11,12 @@ require (
 	github.com/go-playground/validator/v10 v10.4.1
 	github.com/gorilla/handlers v1.5.1
 	github.com/gorilla/mux v1.8.0
+	github.com/gorilla/websocket v1.4.2
 	github.com/imdario/mergo v0.3.11 // indirect
 	github.com/joho/godotenv v1.3.0
 	github.com/pelletier/go-toml v1.8.1 // indirect
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
+	golang.org/x/net v0.0.0-20200602114024-627f9648deb9
 	golang.org/x/sys v0.0.0-20210113181707-4bcb84eeeb78 // indirect
 	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543
 	gorm.io/driver/postgres v1.0.5
--- service.notifications/main.go
+++ service.notifications/main.go
@@ -1,7 +1,91 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
+)
+
+var address = ":9090"
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func reader(connection *websocket.Conn) {
+	for {
+		messageType, messageBytes, err := connection.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		log.Println(string(messageBytes))
+
+		if err := connection.WriteMessage(messageType, messageBytes); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+	// allow any connection to ws endpoint... TODO: fix
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
+	websocket, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println("Client successfully connected to WebSocket...")
+	reader(websocket)
+}
 
 func main() {
-	fmt.Println("Hello World")
+	logger := log.New(os.Stdout, "notifications ", log.LstdFlags)
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", wsEndpoint)
+
+	// create a new server
+	server := http.Server{
+		Addr:         address,
+		Handler:      router,
+		ErrorLog:     logger,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second, // keeps the connection open for specified time
+	}
+
+	// start the server
+	go func() {
+		logger.Println("Starting server on port", address)
+
+		err := server.ListenAndServe()
+		if err != nil {
+			logger.Printf("Error starting server: %s\n", err)
+			os.Exit(1)
+		}
+	}()
+
+	// gracefully shutdown the server and catch interrupt or kill
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, os.Kill)
+
+	// block until signal is received
+	sig := <-c
+	log.Println("Got signal:", sig)
+
+	// gracefully shutdown the server, waiting max 30 seconds for current operations to complete
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	server.Shutdown(ctx)
 }
