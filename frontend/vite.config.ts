a5bd52051812583679d9839d38a8041421205692
FlorianWoelki <FlorianWoelki@gmx.de>
2021-04-17 14:26:15 +0200

    ref(frontend): update tsconfig

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
+// @ts-ignore
 import reactSvgPlugin from 'vite-plugin-react-svg';
 
 // https://vitejs.dev/config/
