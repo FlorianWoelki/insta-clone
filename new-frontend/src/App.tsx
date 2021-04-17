37dcb4ed7a6ae485b9b4ce70507cd90e6fa3b283
FlorianWoelki <FlorianWoelki@gmx.de>
2021-04-17 12:01:24 +0200

    feat: add new frontend with react

---
+++ new-frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-2xl">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
