dec9c19b598c7f5ad1312b03110699c97b530b37
FlorianWoelki <FlorianWoelki@gmx.de>
2021-04-22 08:54:21 +0200

    ref(avatar): remove children prop

--- frontend/src/components/Avatar.tsx
+++ frontend/src/components/Avatar.tsx
@@ -5,24 +5,23 @@ type AvatarProps = {
   noActiveStories?: boolean;
 };
 
-const Avatar: FunctionComponent<AvatarProps> = (props): JSX.Element => {
+const Avatar: FunctionComponent<AvatarProps> = ({
+  noActiveStories,
+  imageSize,
+}): JSX.Element => {
   return (
     <div className="flex items-center justify-center">
       <div
         className={`inline-block p-0.5 rounded-full ${
-          props.noActiveStories
+          noActiveStories
             ? 'bg-gray-300'
             : 'bg-gradient-to-tr from-yellow-400 to-pink-600'
-        }`}
-      >
+        }`}>
         <a href="#" className="relative block p-1 bg-white rounded-full">
           <img
-            className={`w-${props.imageSize} h-${props.imageSize} rounded-full`}
+            className={`w-${imageSize} h-${imageSize} rounded-full`}
             src="https://placekitten.com/200/200"
-            alt="pb-avatar"
-          ></img>
-
-          {props.children}
+            alt="pb-avatar"></img>
         </a>
       </div>
     </div>
