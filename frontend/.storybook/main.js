93663ddc71e4ee1cdb08debaa2c8eba73025b6a0
FlorianWoelki <FlorianWoelki@gmx.de>
2021-04-24 10:46:52 +0200

    fix(storybook): warning with addon essentials and controls

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -3,9 +3,9 @@ const path = require('path');
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
+    '@storybook/addon-essentials',
     '@storybook/addon-controls',
     '@storybook/addon-links',
-    '@storybook/addon-essentials',
     {
       name: '@storybook/addon-postcss',
       options: {
