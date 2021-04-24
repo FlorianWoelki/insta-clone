e3e5e119f3fd7cc308057f31b45355012c0b7d1d
FlorianWoelki <FlorianWoelki@gmx.de>
2021-04-24 11:21:38 +0200

    feat(frontend): add Search story

---
+++ frontend/src/stories/Search.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Search from '../components/Search';
+
+export default {
+  title: 'Components/Search',
+  component: Search,
+};
+
+export const Default = () => <Search></Search>;
